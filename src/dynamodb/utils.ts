69985f3dac4651dd26c1192dfe9b55edf59099c0
Cauã Kath <80467897+CauaKath@users.noreply.github.com>
2025-10-17 09:14:39 -0300

    Merge pull request #305 from simplificamais/feat/receive-region-and-endpoint-on-get-client-options
    feat: receive region and endpoint on get client options

--- src/dynamodb/utils.ts
+++ src/dynamodb/utils.ts
@@ -1,13 +1,17 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
+import { GetDynamoDBClientOptions } from "./dynamodb.types"
 
-export function getDynamoDBClient(port = 8000) {
+export function getDynamoDBClient(options: GetDynamoDBClientOptions = {}) {
   const dynamoInstance = new DynamoDBClient({
     ...(process.env.IS_OFFLINE ? {
-      endpoint: `http://localhost:${port}`,
+      endpoint: options.localEndpoint || 'http://localhost:8000',
       region: 'localhost'
-    } : {})
-  })
+    } : {
+      endpoint: options.endpoint || undefined,
+      region: options.region || 'sa-east-1'
+    }),
+  });
 
   const client = DynamoDBDocument.from(dynamoInstance, {
     marshallOptions: { removeUndefinedValues: true }
